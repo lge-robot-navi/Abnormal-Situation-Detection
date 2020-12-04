3f9a737ca4bbc1191950c3ab0882b3c36b6218f9
JinHa <leejinha829@gmail.com>
2020-12-04 17:49:44 +0900

    add reference

--- 3_Code/OSR_ped_anomaly/model_anomaly.py
+++ 3_Code/OSR_ped_anomaly/model_anomaly.py
@@ -1,3 +1,31 @@
+
+""" #
+@file		model_anomaly.py
+@date   	Aug 13, 2020
+@author 	Jin-ha Lee (jh_lee@etri.re.kr)
+@brief		This file include ROS node for publishing fixed agent anomaly score for pedestrain.
+
+Copyright (C) 2019  <Jin-ha Lee>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+
+
+
+
 import numpy as np
 import tensorflow.keras as ke
 import random
